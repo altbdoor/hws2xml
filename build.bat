86de5b66a51aaed975ea85b2e4ef328cc227a73c
altbdoor <lancersupraskyline@gmail.com>
2016-12-27 11:39:19 +0800

    so mono was a batch file too

--- build.bat
+++ build.bat
@@ -1,8 +1,7 @@
 @echo off
 
 set csc_path=C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe
-set mcs_path=C:\Program Files (x86)\Mono\bin\mcs
-dir "C:\Program Files (x86)\Mono\bin"
+set mcs_path=C:\Program Files (x86)\Mono\bin\mcs.bat
 
 echo ----------------------------------------
 echo Building Windows exe...
@@ -16,7 +15,7 @@ echo ----------------------------------------
 echo Building Mono exe...
 echo ----------------------------------------
 
-"%mcs_path%" -out:hws2xml.exe -r:System -r:System.Drawing -r:System.Xml.Linq Main.cs OpenTK.cs TiltedEngine.cs
+call "%mcs_path%" -out:hws2xml.exe -r:System -r:System.Drawing -r:System.Xml.Linq Main.cs OpenTK.cs TiltedEngine.cs
 7z a mono_hws2xml.zip hws2xml.exe > nul
 del hws2xml.exe
 
