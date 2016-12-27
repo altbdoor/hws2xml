b95b7a6e80978576d7ae27666afc8f472f492e0f
altbdoor <lancersupraskyline@gmail.com>
2016-12-27 10:52:57 +0800

    lets build in mono too

--- build.bat
+++ build.bat
@@ -1,11 +1,21 @@
 @echo off
 
-echo Building exe...
-echo ----------------------------------------
-
 set csc_path=C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe
 
-%csc_path% /t:exe /out:hws2xml.exe Main.cs OpenTK.cs TiltedEngine.cs
+echo ----------------------------------------
+echo Building Windows exe...
+echo ----------------------------------------
+
+%csc_path% /optimize+ /t:exe /out:hws2xml.exe Main.cs OpenTK.cs TiltedEngine.cs
+7z a win_hws2xml.zip hws2xml.exe > nul
+del hws2xml.exe
 
 echo ----------------------------------------
+echo Building Mono exe...
+echo ----------------------------------------
+
+mcs -out:hws2xml.exe -r:System -r:System.Drawing -r:System.Xml.Linq Main.cs TiltedEngine.cs OpenTK.cs
+7z a mono_hws2xml.zip hws2xml.exe > nul
+del hws2xml.exe
+
 echo Done!
