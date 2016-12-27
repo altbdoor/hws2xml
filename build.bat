7ae1e1540e3c2e2da7216990bdf59a35785f6a7a
altbdoor <lancersupraskyline@gmail.com>
2016-12-27 11:25:09 +0800

    does mono even exist

--- build.bat
+++ build.bat
@@ -1,13 +1,14 @@
 @echo off
 
-set csc_path="C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe"
-set mcs_path="C:\Program Files (x86)\Mono\bin\mcs"
+set csc_path=C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe
+set mcs_path=C:\Program Files (x86)\Mono\bin\mcs
+dir "C:\Program Files (x86)\Mono\bin"
 
 echo ----------------------------------------
 echo Building Windows exe...
 echo ----------------------------------------
 
-%csc_path% /optimize+ /t:exe /out:hws2xml.exe Main.cs OpenTK.cs TiltedEngine.cs
+"%csc_path%" /optimize+ /t:exe /out:hws2xml.exe Main.cs OpenTK.cs TiltedEngine.cs
 7z a win_hws2xml.zip hws2xml.exe > nul
 del hws2xml.exe
 
@@ -15,7 +16,7 @@ echo ----------------------------------------
 echo Building Mono exe...
 echo ----------------------------------------
 
-%mcs_path% -out:hws2xml.exe -r:System -r:System.Drawing -r:System.Xml.Linq Main.cs OpenTK.cs TiltedEngine.cs
+"%mcs_path%" -out:hws2xml.exe -r:System -r:System.Drawing -r:System.Xml.Linq Main.cs OpenTK.cs TiltedEngine.cs
 7z a mono_hws2xml.zip hws2xml.exe > nul
 del hws2xml.exe
 
